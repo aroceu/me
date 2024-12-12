25a82b52a16910ced2bcb41d4560c1af6e9e1fe9
Arrow Sparrow <dev@aroceu.cloud>
2024-12-12 11:24:21 -0500

    Added ~kinlist for fun

--- index.php
+++ index.php
@@ -170,6 +170,7 @@ nav a:hover, nav a:focus{
 
 <nav>
 	<a href="https://aroceu.com">home</a>
+	<a href="https://aroceu.com/me/kin">kins</a>
 </nav>
 
 </div>
---
+++ kin/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    
+    <head>
+        
+        <title>aroceu (me)</title>
+        
+        <meta name="robots" content="noai, noimageai">
+
+        <meta name="viewport" content="initial-scale=1.0">
+        <meta name="description" content="not an internet personality"/>
+    <link rel="shortcut icon" type="image/x-icon" href="https://aroceu.com/fav.ico" />
+<link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..700&family=Cookie&display=swap" rel="stylesheet">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font: normal 100%/160% 'Inconsolata', 'Consolas', monospace;
+  background-color: #fff;
+background-image:
+linear-gradient(90deg, transparent 79px, #abced4 79px, #abced4 81px, transparent 81px),
+linear-gradient(#eee .1em, transparent .1em);
+background-size: 100% 1.2em;
+  color: #555;
+              text-shadow: 0.02em 0.02em 0 #bdbdbd;
+
+}
+
+.container{
+	margin-top: 5em;
+	width: 90%;
+}
+
+#blurb{
+	padding: 1em;
+	color: #999;
+	background: #fff;
+	  border: 1px dashed #525276;
+	  width: 30em;
+	  float: right;
+	  box-shadow: 0.2em 0.2em 0 #ccc;
+}
+
+h1{
+	text-transform: lowercase;
+	font: normal 350% 'Cookie';
+	margin: 0;
+	padding: 10px 10px 0 0;
+	text-align: right;
+	color: #5C98B2;
+            text-shadow: 0.02em 0.02em 0 #bdbdbd;
+}
+
+i{
+	color: #5C98B2;
+}
+
+nav{
+	text-align: center;
+}
+
+nav a:link, nav a:visited{
+	background: #5C98B2;
+	padding: 10px;
+	color: #fafafa!important;
+	text-transform: uppercase;
+	letter-spacing: 2px;
+	text-decoration: none;
+}
+
+nav a:hover, nav a:focus{
+	background: #999;
+}
+
+#list{
+	  margin-left: 15em;
+	  text-align: center;
+	  width: 35em;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 4px;
+  width: 100%;
+}
+
+.kin {
+  flex: 25%;
+  max-width: 25%;
+  padding: 20px;
+}
+
+.kin img{
+	border-radius: 150px;
+	height: 100px;
+	width: 100px;
+	
+}
+
+.kin img.noticon{
+	border-radius: 0;
+}
+
+.kin img {
+  vertical-align: middle;
+  
+}
+
+
+
+</style>
+</head>
+<body>
+
+<div class="container">
+
+  	<div id="blurb">
+
+<h1>Kinnies</h1>
+
+<p>I don't identify strongly with ~kin stuff, particularly for fictional characters; but that doesn't stop me from <i>feeling</i> like I see myself in these characters. Even for older fandoms and media that I don't care much for anymore, sometimes I see a picture of a character and still get that <i>it's me</i> feeling. More often than not though they're vehicles to help me better understand myself.</p>
+
+<p>Characters I'm most drawn to on a relational level are usually some combination of: blue themed, intellectual, obsessive with being inventive, sensitive, traumatized or depressed, lazy out of inconvenience, eldest siblings, loners, waterborne, and sarcastic/witty.</p>
+
+<p>In order of when I felt kinship, except for the first because I've always felt connected to the ocean.</p>
+
+<nav>
+	<a href="https://aroceu.com">home</a> <a href="https://aroceu.com/me/">personality</a>
+</nav>
+
+</div>
+  
+<div id="list">
+
+  <div class="row">
+
+  	<div class="kin">
+  		<img src="ocean.png">
+  	</div>
+
+  	<div class="kin">
+  		<img src="mark.jpg">
+  	</div>
+	
+		<div class="kin">
+  		<img src="spiderman.png">
+  	</div>
+
+  	<div class="kin">
+  		<img src="manami.jpg">
+  	</div>
+
+  	<div class="kin">
+  		<img src="miles.png">
+  	</div>
+
+  	<div class="kin">
+  		<img src="haru.png">
+  	</div>
+
+  	<div class="kin">
+  		<img src="kunimi.jpg">
+  	</div>
+
+  	<div class="kin">
+  		<img src="rocket.png">
+  	</div>
+
+  	<div class="kin">
+  		<img src="cress.png" class="noticon">
+  	</div>
+
+  	<div class="kin">
+  		<img src="l.jpg">
+  	</div>
+
+   <div class="kin">
+  		<img src="violet.jpg">
+  	</div>
+
+  	<div class="kin">
+  		<img src="raven.png">
+  	</div>
+
+  	<div class="kin">
+  		<img src="violet.png">
+  	</div>
+
+  	<div class="kin">
+  		<img src="bubbles.png">
+  	</div>
+		
+  </div> 
+
+</div>
+ 
+</div>
+
+</body>
+</html>
+
+
