db3fb5993d77b5a278cabf763a4543f999254232
aroceu <dev@aroceu.cloud>
2025-05-12 23:26:39 -0400

    Added header stuff

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!doctype html>
 <html lang="en">
-<meta name="viewport" content="initial-scale=1.0">
 <head>
 
 <title>aroceu (me)</title>
-<meta name="robots" content="noai, noimageai">
-<meta name="viewport" content="initial-scale=1.0">
-<meta name="description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house."/>
+	<meta name="robots" content="noai, noimageai">
+	<meta charset="UTF-8">
+	<meta name="description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house."/>
+	<meta property="og:title" content="aroceu (me)" />
+	<meta property="og:description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house." />
+	<meta property="og:image" content="https://aroceu.com/assets/preview.png" />
+	<meta property="og:url" content="https://aroceu.com/" />
+	<meta name="viewport" content="initial-scale=1.0, maximum-scale=5, width=device-width">
 <link rel="shortcut icon" type="image/x-icon" href="../assets/fav.ico" />
 <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..700&family=Rochester&display=swap" rel="stylesheet">
 <script type="text/javascript">
